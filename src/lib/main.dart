2d811190608f1398e79bb8fd87c0c362e47fd727
Jan Golebiowski <bowski23@hotmail.de>
2022-06-16 19:25:46 +0200

    Merge with Furkan, WIP
    Co-authored-by: Furkan Tas <furkan000@users.noreply.github.com>

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:ddcapp/object_detector_view.dart';
 import 'package:ddcapp/provider/location_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -20,5 +21,5 @@ Future<void> main() async {
       create: (context) => LocationProvider(),
       child: GoogleMapPage(),
     )
-  ], child: MaterialApp(home: const CameraHomePage())));
+  ], child: MaterialApp(home: ObjectDetectorView())));
 }
